67645d49320143871c24f662be938a7595ad04d8
mchoudhary11 <mridhul.choudhary11@gmail.com>
2022-10-19 15:50:10 +0530

    .

--- src/features/productsApi.js
+++ src/features/productsApi.js
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const productsApi = createApi({
   reducerPath: "productsApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://product-list1.herokuapp.com/",
+  }),
   endpoints: (builder) => ({
     getAllProducts: builder.query({
       query: () => "products",
